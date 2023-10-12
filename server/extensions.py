603af4df5c9da5eb1fd6e9f9b4a4f3b8b3737510
Beliya470 <anneanziay78@gmail.com>
2023-10-12 16:35:03 +0300

    colf

--- server/extensions.py
+++ server/extensions.py
@@ -2,6 +2,14 @@ from flask_sqlalchemy import SQLAlchemy
 import marshmallow
 from flask_login import LoginManager
 
+def _include_sqlalchemy(obj, query_class=None):
+    from sqlalchemy import orm
+    query_class = obj.Query = orm.Query if query_class is None else query_class
+    for module in 'engine url Model query session'.split():
+        setattr(obj, module, getattr(orm, module))
+
+SQLAlchemy._include_sqlalchemy = _include_sqlalchemy
+
 db = SQLAlchemy()
 ma = marshmallow
 login_manager = LoginManager()
