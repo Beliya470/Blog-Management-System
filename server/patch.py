cc33f204b5ed1480259cb05e7830911d020342d0
Beliya470 <anneanziay78@gmail.com>
2023-10-12 17:02:40 +0300

    colf

--- server/patch.py
+++
@@ -1,6 +0,0 @@
-def apply_patch():
-    try:
-        import flask_sqlalchemy
-        delattr(flask_sqlalchemy.SQLAlchemy, '_include_sqlalchemy')
-    except Exception as e:
-        print(f"Failed to apply patch: {e}")
